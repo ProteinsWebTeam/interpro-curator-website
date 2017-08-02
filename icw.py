fc9b39f97e9100e11c6eb687063d5814095715bf
Matthias Blum <mat.blum@gmail.com>
2017-08-02 15:38:37 +0100

    Handle concurrent requests

--- icw.py
+++ icw.py
@@ -793,4 +793,4 @@ def api_protein(protein_ac):
 
 
 if __name__ == '__main__':
-    app.run(host=app.config['HOST'], port=app.config['PORT'], debug=app.config['DEBUG'])
+    app.run(host=app.config['HOST'], port=app.config['PORT'], debug=app.config['DEBUG'], threaded=True)
